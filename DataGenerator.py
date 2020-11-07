e1de7e5a21a398f74345f1399668962120619422
geonhui <kgh905@gmail.com>
2020-11-07 18:09:24 +0900

    add height, weight, gender

--- DataGenerator.py
+++ DataGenerator.py
@@ -19,8 +19,8 @@ for year in range(startYear, endYear+1):
     os.mkdir(f"{dataPath}/{year}")
     for month in range(startMonth, EndMonth+1):
         os.mkdir(f"{dataPath}/{year}/{month}")
-        for userIndex in range(1, random.randrange(10, 50)):
-            #f = open(f"{dataPath}/{year}/{month}/user{userIndex}.json", 'w+')
+        for userIndex in range(1, random.randint(10, 50)):
+            # f = open(f"{dataPath}/{year}/{month}/user{userIndex}.json", 'w+')
             # f.write(f"{year:{year}}")
             # f.close()
             with open(f"{dataPath}/{year}/{month}/user{userIndex}.json", 'w+') as json_file:
@@ -28,5 +28,8 @@ for year in range(startYear, endYear+1):
                     "birthYear": year,
                     "birthMonth": month,
                     "name": f"User{userIndex}",
+                    "height": random.randint(50, 100),
+                    "weight": round(random.uniform(1.5, 4.0), 2),
+                    "gender": ("female" if random.randint(1, 2) == 1 else "male")
                 }
                 json.dump(user, json_file)
